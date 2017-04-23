9c14f47db2dd80132c979a8338f37d06dad4666f
Andrew Imm <andrewi@fb.com>
2017-04-23 14:58:56 -0700

    Pass authoptions to cloud function, fixes #11

--- src/Cloud.js
+++ src/Cloud.js
@@ -3,12 +3,18 @@
 import {decode, encode} from './WireFormat';
 
 import type App from './App';
+import type {AuthOptions} from './Types';
 
 type CloudResponse = {
   result?: any,
 };
 
-export default function Cloud(app: App, name: string, data: any = {}): Promise<any> {
+export default function Cloud(
+  app: App,
+  name: string,
+  data: any = {},
+  options: AuthOptions = {}
+): Promise<any> {
   if (typeof name !== 'string' || name.length === 0) {
     throw new TypeError('Cloud function name must be a string.');
   }
@@ -18,7 +24,7 @@ export default function Cloud(app: App, name: string, data: any = {}): Promise<a
   return app.client.post(
     'functions/' + name,
     payload,
-    {}
+    options
   ).then(({response}) => {
     const decoded: CloudResponse = (decode(response): any);
     if (decoded && decoded.hasOwnProperty('result')) {
